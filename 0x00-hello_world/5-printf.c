f53d163d7000e7fa18754f443c44412b8ae3cbe7
Naledimabanga <mabangantombifuthi@gmail.com>
2023-02-16 03:18:36 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,15 +3,10 @@
 /**
  * main - Entry point
  *
- *Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("size of a char: %lu byte(s)\n", sizeof(char));
-	printf("size of a char: %lu byte(s)\n", sizeof(int));
-	printf("size of a char: %lu byte(s)\n", sizeof(long int));
-	printf("size of a char: %lu byte(s)\n", sizeof(long long int));
-	printf("size of a char: %lu byte(s)\n", sizeof(float));
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
