463798175903f311adfeae751edd7878e1de88c2
Naledimabanga <mabangantombifuthi@gmail.com>
2023-02-21 22:13:04 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,18 +1,66 @@
 #include "main.h"
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_spaces - This functions prints commas and spaces
+ * @n: is a intenger define the number of spaces
+(* a blank line
+* Description: this function prints spaces)?
+* Return: no return a void function.
  */
-int main(void)
+void print_spaces(int n)
 {
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-    return (0);
+	int i;
+
+	_putchar(',');
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(' ');
+	}
 }
+/**
+ * print_times_table - This functions prints times table
+ * @n: the table to print
+(* a blank line
+ * Description: this function print times table)?
+(* section header: the header file for this function is holberton.h)*
+ * Return: no return a void function.
+ */
+void print_times_table(int n)
+{
+	int table, number;
+
+	if (n < 0 || n > 15)
+		return;
+	for (table = 0; table <= n; table++)
+	{
+		for (number = 0; number <= n; number++)
+		{
+			int r;
+
+			r = table * number;
+			if (number && r < 10)
+				print_spaces(3);
+			else if (r >= 10 && r < 100)
+				print_spaces(2);
+			else if (r >= 100)
+				print_spaces(1);
+			if (r >= 10 && r < 100)
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+			else if (r >= 100)
+			{
+				_putchar((r / 100) + '0');
+				_putchar(((r / 10) % 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+			else
+			{
+				_putchar(r + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
